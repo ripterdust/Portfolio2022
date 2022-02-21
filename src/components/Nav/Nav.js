983ce91432a3f3a21035f5decca1420bbb08660f
ripterdust <bryantello2010@hotmail.com>
2022-02-21 11:40:14 -0600

    added the nav component tags

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,9 +1,14 @@
+import { Link } from 'react-router-dom';
 import './Nav'
 
 export const Nav = () => {
     
     return <div>
-        asdfasdf
+        <Link to='/'>_hello</Link>
+        <Link to='/about'>_about</Link>
+        <Link to='/project'>_projects</Link>
+
+
     </div>
 
 };
