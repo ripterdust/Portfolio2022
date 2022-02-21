d9a82c4d3c9df0b1d13826d4275ced1839d825bf
ripterdust <bryantello2010@hotmail.com>
2022-02-21 15:17:32 -0600

    Added the dictionary and starting to build the commands

--- src/components/Console.js
+++ src/components/Console.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import { listOfCommands } from '../commands/listOfCommands'
 
 export const Console = ({ command }) => {
-  return (
-    <div>{ command }</div>
-  )
+
+  const { list } = listOfCommands;
+
+  let autocomplete = list.filter( word => word.includes(command));
+  return <div>
+    {
+      autocomplete != '' ? `Maybe you want to say: ${autocomplete}.` : 'Type "help" to see the commands.'
+    }
+  </div>
 }
