09bc3af607693b547e5699e054cf86351f8d3ab6
ripterdust <bryantello2010@hotmail.com>
2022-02-21 14:45:36 -0600

    Added the console functionality

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,10 +1,20 @@
+import { useState } from "react";
+import { Console } from "./Console";
+
 export const Hello = () => {
+
+  const [command, setCommand] = useState('');
+
+
   return (
     <div className="hello">
       <div className="command">
-        $<input type="text" placeholder="Type your command..." />
+        <div className="sign">
+          $
+        </div>
+        <input type="text" value={command} onChange={(e) => setCommand(e.target.value)} placeholder="Type your command..." />
       </div>
-
+      { command == '' ? 'Vacío' : <Console/> }
     </div>
   )
 }
