cd114626efa3a463a74b2d93bfcb3474e07360ba
ripterdust <bryantello2010@hotmail.com>
2022-02-21 12:04:44 -0600

    Added the buttons component

---
+++ src/components/Buttons.js
@@ -0,0 +1,7 @@
+export const Buttons = () => {
+    return <div className="buttons">
+        <div className="red">a</div>
+        <div className="yellow">a</div>
+        <div className="green">a</div>
+    </div>
+}
