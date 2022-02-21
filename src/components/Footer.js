cd1595f80ca1c654f8d50f506b379f8323aa1316
ripterdust <bryantello2010@hotmail.com>
2022-02-21 14:11:35 -0600

    Added the footer data

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Footer = () => {
+
+  const [date, setDate] = useState('1:37 21/02/2022');
+  
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <div className="social">
+        <div className='item' >Find me in:</div>
+        <a className='item' href="">link</a>
+        <a className='item' href="">link</a>
+        <a className='item' href="">link</a>
+      </div>
+      <div className="time">
+        {
+          date
+        }
+      </div>
+    </div>
   )
 }
