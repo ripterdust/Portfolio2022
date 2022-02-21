c4d92a7a5d345df5b88fe2839673014a4ee83a71
ripterdust <bryantello2010@hotmail.com>
2022-02-21 12:04:25 -0600

    Added the buttons component

--- src/router/MainRouter.js
+++ src/router/MainRouter.js
@@ -1,9 +1,11 @@
 import { BrowserRouter } from "react-router-dom"
+import { Buttons } from "../components/Buttons"
 import { Nav } from "../components/Nav/Nav"
 
 export const MainRouter = () => {
     return <div className="terminal">
         <BrowserRouter>
+            <Buttons/>
             <Nav/>  
 
         </BrowserRouter>
