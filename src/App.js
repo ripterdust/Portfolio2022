7f445918c5a54b220bf1f099d2f1e263a5b9c446
ripterdust <bryantello2010@hotmail.com>
2022-02-22 10:51:22 -0600

    fixed the terminal size

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import { Desktop } from "./components/Desktop";
+import { TaskBar } from "./components/TaskBar";
 import { MainRouter } from "./router/MainRouter";
 
 function App() {
   return <div className="window">
+
+      <Desktop/>
       <MainRouter/>
+
+      <TaskBar/>
   </div>
 }
 
