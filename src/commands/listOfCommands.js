429d5332cf02fc12c704201181e126de3a8798e3
ripterdust <bryantello2010@hotmail.com>
2022-02-21 18:57:33 -0600

    Fixed the desestructuration bug

--- src/commands/listOfCommands.js
+++ src/commands/listOfCommands.js
@@ -1,4 +1,4 @@
-const listOfCommands = {
+export const listOfCommands = {
     list : ['hello', 'help', 'fakefetch', 'date', 'social', 'cv', 'curriculum', 'resume', 'github', 'linkedin'],
     commands : {
         hello: {
