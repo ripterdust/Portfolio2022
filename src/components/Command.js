6d7c6d591aa941c4c00f82a30da37934ac6b67ca
ripterdust <bryantello2010@hotmail.com>
2022-02-21 20:50:11 -0600

    Terminal working

--- src/components/Command.js
+++ src/components/Command.js
@@ -1,8 +1,25 @@
+import { useContext } from "react";
+import { DataContext } from "../context/useData";
 
+export const Command = ({text }) => {
 
-export const Command = ({text}) => {
+  const [ , setState ] = useContext(DataContext);
+
+  
   if(text){
-    return 'Completado'
+    if(text == 'help'){
+      return 'help'
+    }else if(text == 'social'){
+      return 'www.github.com'
+    }else{
+
+      setState({
+        command: '',
+        clearScreen: false
+      })
+      return 'Nada'
+    }
+
   }else{
       return 'An error has ocurred'
   }
