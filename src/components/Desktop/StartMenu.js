4f9a793538f135e52a8fc5b60547f980318ab9a7
ripterdust <bryantello2010@hotmail.com>
2022-02-22 12:53:03 -0600

    fixed the menu

--- src/components/Desktop/StartMenu.js
+++ src/components/Desktop/StartMenu.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const StartMenu = () => {
   return (
-    <div className='startMenu open' id="menu">
+    <div className='startMenu close' id="menu">
       <div className='Bg'>
         Acá irá la data
       </div>
