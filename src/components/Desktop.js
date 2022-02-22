787cb45bf4b7009030bea41ec7ec22492250d1ec
ripterdust <bryantello2010@hotmail.com>
2022-02-22 10:53:43 -0600

    Starting the layout and adding the desktop styles

--- src/components/Desktop.js
+++ src/components/Desktop.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const Desktop = () => {
   return (
-    <div>Desktop</div>
+    <div className='desktop'>Desktop</div>
   )
 }
--- src/components/TaskBar.js
+++ src/components/TaskBar.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const TaskBar = () => {
   return (
-    <div>TaskBar</div>
+    <div className='taskbar'>TaskBar</div>
   )
 }
