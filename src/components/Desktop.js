7c972e8e87c6c79c6bc5dc7f43d8bd92aadefc50
ripterdust <bryantello2010@hotmail.com>
2022-02-22 11:16:56 -0600

    Added the first icon

--- src/components/Desktop.js
+++ src/components/Desktop.js
@@ -2,6 +2,13 @@ import React from 'react'
 
 export const Desktop = () => {
   return (
-    <div className='desktop'>Desktop</div>
+    <div className='desktop'>
+      <div className="icons">
+        <div className="icon">
+          <i class="fa-solid fa-terminal terminal-icon"></i>
+          Portfolio<br />[Terminal]
+        </div>
+      </div>
+    </div>
   )
 }
