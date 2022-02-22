18f17bdf1fbcb7d47992c63a2071ad8e055ac60d
ripterdust <bryantello2010@hotmail.com>
2022-02-22 13:55:14 -0600

    Added the pdf icon

--- src/components/Desktop/Files.js
+++ src/components/Desktop/Files.js
@@ -5,7 +5,7 @@ export const Files = () => {
   return (
     <div id='files' className='hiden animate__animated animate__fadeIn' >
       <Buttons classToHide = { 'files' } />
-
+      <i class="fa-solid fa-file-pdf"></i>
     </div>
   )
 }
