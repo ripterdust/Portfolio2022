403de83cc495fc189e4a8f320bd8d30893a36545
ripterdust <bryantello2010@hotmail.com>
2022-02-22 11:26:28 -0600

    Finished the main open terminal animation

--- src/components/Desktop.js
+++ src/components/Desktop.js
@@ -1,10 +1,17 @@
 import React from 'react'
 
 export const Desktop = () => {
+
+  const handleClick = (id, elemClass) => {
+    const element = document.querySelector(`#${id}`).classList;
+    element.remove('hiden');
+    element.add(elemClass);
+  }
+
   return (
     <div className='desktop'>
       <div className="icons">
-        <div className="icon">
+        <div className="icon" onClick={() => handleClick('terminal', 'terminal')}>
           <i class="fa-solid fa-terminal terminal-icon"></i>
           Portfolio<br />[Terminal]
         </div>
