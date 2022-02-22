af03a05c7d19ea082df50502e42a5d7328441c8d
ripterdust <bryantello2010@hotmail.com>
2022-02-22 10:47:06 -0600

    Added the close action

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,7 +1,14 @@
 export const Buttons = () => {
+
+    const handleClick = () => {
+        let terminal = document.querySelector('#terminal')
+        terminal.remove('terminal')
+
+    }
+
     return <div className="top">
         <div className="buttons">
-            <div className="button red"></div>
+            <div className="button red" onClick={ handleClick }></div>
             <div className="button yellow"></div>
             <div className="button green"></div>
         </div>
--- src/router/MainRouter.js
+++ src/router/MainRouter.js
@@ -7,7 +7,7 @@ import { Nav } from "../components/Nav"
 import { Projects } from "../components/Projects"
 
 export const MainRouter = () => {
-    return <div className="terminal">
+    return <div className="terminal" id="terminal">
         <BrowserRouter>
             <Buttons/>
             <Nav/>  
