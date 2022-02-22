eb89d695253c7bb171e6df58b73b8fafd5a1384d
ripterdust <bryantello2010@hotmail.com>
2022-02-21 19:14:26 -0600

    Added the new instructions

--- src/components/Console.js
+++ src/components/Console.js
@@ -8,8 +8,18 @@ export const Console = ({ command }) => {
   let autocomplete = list.filter( word => word.includes(command));
 
   return <div className='animate__animated animate__fadeIn'>
+    
     {
-      autocomplete != '' ? `Maybe you want to say: ${autocomplete}.` : 'Type "help" to see the commands.'
+      autocomplete != '' && `Maybe you want to write ${autocomplete}`
     }
+    <br />
+    <br />
+    Instructions: 
+    <br />
+    <br /> 
+    * Type "help" to see the commands. 
+    <br /> 
+    * Press enter to submit<br/>
+
   </div>
 }
