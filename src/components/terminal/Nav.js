8f526630c40799446083aeb6944cd2b11064be4b
ripterdust <bryantello2010@hotmail.com>
2022-02-22 19:54:42 -0600

    Fixed the isActive warning

--- src/components/terminal/Nav.js
+++ src/components/terminal/Nav.js
@@ -6,9 +6,9 @@ export const Nav = () => {
     
     
     return <nav>
-        <NavLink activeClassName="active" exact to='/'>_hello</NavLink>
-        <NavLink activeClassName="active" to='/about'>_about</NavLink>
-        <NavLink activeClassName="active" to='/project'>_projects</NavLink>
+        <NavLink className={({isActive}) => isActive && 'active'} exact to='/'>_hello</NavLink>
+        <NavLink className={({isActive}) => isActive && 'active'} to='/about'>_about</NavLink>
+        <NavLink className={({isActive}) => isActive && 'active'} to='/project'>_projects</NavLink>
     </nav>
 
 };
