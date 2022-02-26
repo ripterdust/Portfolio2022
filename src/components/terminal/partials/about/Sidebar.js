3a353cbe482c638f5db8f18b54e93bf54eeded15
ripterdust <bryantello2010@hotmail.com>
2022-02-26 14:44:51 -0600

    Added the color classes

--- src/components/terminal/partials/about/Sidebar.js
+++ src/components/terminal/partials/about/Sidebar.js
@@ -6,10 +6,10 @@ export const Sidebar = () => {
       Explorer: <i class="fa-solid fa-folder-tree"></i>
       <ul className='parentList'>
         <li><i class="fa-solid fa-folder-open"></i> React</li>
-        <li><i class="fa-solid fa-folder-open"></i> Node.js</li>
-        <li><i class="fa-solid fa-folder-open"></i> Mysql</li>
-        <li><i class="fa-solid fa-folder-open"></i> Jest</li>
-        <li><i class="fa-solid fa-folder-open"></i> Typescript</li>
+        <li><i class="fa-solid fa-folder-open green"></i> Node.js</li>
+        <li><i class="fa-solid fa-folder-open orange"></i> Mysql</li>
+        <li><i class="fa-solid fa-folder-open yellow"></i> Jest</li>
+        <li><i class="fa-solid fa-folder-open red"></i> Typescript</li>
         <ul className='childList'>
           <li><i class="fa-brands fa-markdown"></i> Read.md</li>
           <li><i class="fa-brands fa-markdown"></i> Stay with my dog.md</li>
