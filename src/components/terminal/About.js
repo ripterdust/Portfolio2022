917875d8a740a130122daf82b283e92c0eda2c54
ripterdust <bryantello2010@hotmail.com>
2022-02-26 14:31:42 -0600

    Added the AboutMe component

--- src/components/terminal/About.js
+++ src/components/terminal/About.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import { AboutMe } from './partials/about/AboutMe'
 import { Sidebar } from './partials/about/Sidebar'
 
 export const About = () => {
   return (
     <div className='animate__animated animate__fadeIn about'>
 
-
+      <AboutMe/>
       <Sidebar/>
     </div>
   )
