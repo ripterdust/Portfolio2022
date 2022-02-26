db581cc1fcc818a3e533c759e7f777f8f57856ab
ripterdust <bryantello2010@hotmail.com>
2022-02-26 14:43:46 -0600

    Added the list of about

--- src/components/terminal/partials/about/Sidebar.js
+++ src/components/terminal/partials/about/Sidebar.js
@@ -4,9 +4,21 @@ export const Sidebar = () => {
   return (
     <div className='sidebar'>
       Explorer: <i class="fa-solid fa-folder-tree"></i>
-      <ul>
-        <li></li>
+      <ul className='parentList'>
+        <li><i class="fa-solid fa-folder-open"></i> React</li>
+        <li><i class="fa-solid fa-folder-open"></i> Node.js</li>
+        <li><i class="fa-solid fa-folder-open"></i> Mysql</li>
+        <li><i class="fa-solid fa-folder-open"></i> Jest</li>
+        <li><i class="fa-solid fa-folder-open"></i> Typescript</li>
+        <ul className='childList'>
+          <li><i class="fa-brands fa-markdown"></i> Read.md</li>
+          <li><i class="fa-brands fa-markdown"></i> Stay with my dog.md</li>
+          <li><i class="fa-brands fa-markdown"></i> Engeniering.md</li>
+          <li><i class="fa-brands fa-markdown"></i> Travels.md</li>
+
+        </ul>
       </ul>
+
 
     </div>
   )
