314575c7bff37702c5e07baf4cdb5cc75b6dbc9f
ripterdust <bryantello2010@hotmail.com>
2022-02-26 14:29:05 -0600

    Added the about page and starting the styles on the sidebar

--- src/components/terminal/About.js
+++ src/components/terminal/About.js
@@ -3,7 +3,7 @@ import { Sidebar } from './partials/about/Sidebar'
 
 export const About = () => {
   return (
-    <div className='animate__animated animate__fadeIn'>
+    <div className='animate__animated animate__fadeIn about'>
 
 
       <Sidebar/>
