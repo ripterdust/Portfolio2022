7a3b67678e8a3d919d43a478f1980bd7b6db6c50
ripterdust <bryantello2010@hotmail.com>
2022-02-26 16:37:02 -0600

    Fixed the className bug

--- src/components/terminal/partials/about/Sidebar.js
+++ src/components/terminal/partials/about/Sidebar.js
@@ -3,18 +3,18 @@ import React from 'react'
 export const Sidebar = () => {
   return (
     <div className='sidebar'>
-      Explorer: <i class="fa-solid fa-folder-tree"></i>
+      Explorer: <i className="fa-solid fa-folder-tree"></i>
       <ul className='parentList'>
-        <li><i class="fa-solid fa-folder-open blue"></i> React</li>
-        <li><i class="fa-solid fa-folder-open green"></i> Node.js</li>
-        <li><i class="fa-solid fa-folder-open orange"></i> Mysql</li>
-        <li><i class="fa-solid fa-folder-open yellow"></i> Jest</li>
-        <li><i class="fa-solid fa-folder-open red"></i> Typescript</li>
+        <li><i className="fa-solid fa-folder-open blue"></i> React</li>
+        <li><i className="fa-solid fa-folder-open green"></i> Node.js</li>
+        <li><i className="fa-solid fa-folder-open orange"></i> Mysql</li>
+        <li><i className="fa-solid fa-folder-open yellow"></i> Jest</li>
+        <li><i className="fa-solid fa-folder-open red"></i> Typescript</li>
         <ul className='childList'>
-          <li><i class="fa-brands fa-markdown"></i> Read.md</li>
-          <li><i class="fa-brands fa-markdown"></i> Stay with my dog.md</li>
-          <li><i class="fa-brands fa-markdown"></i> Engeniering.md</li>
-          <li><i class="fa-brands fa-markdown"></i> Travels.md</li>
+          <li><i className="fa-brands fa-markdown"></i> Read.md</li>
+          <li><i className="fa-brands fa-markdown"></i> Stay with my dog.md</li>
+          <li><i className="fa-brands fa-markdown"></i> Engeniering.md</li>
+          <li><i className="fa-brands fa-markdown"></i> Travels.md</li>
 
         </ul>
       </ul>
--- src/components/terminal/partials/projects/ProjectCard.js
+++ src/components/terminal/partials/projects/ProjectCard.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 export const ProjectCard = ({ data }) => {
+  console.log(data)
   return (
     <div>ProjectCard</div>
   )
