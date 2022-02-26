0e5c2ce38d38776028fef1a6bafcaf01248b762b
ripterdust <bryantello2010@hotmail.com>
2022-02-26 14:38:45 -0600

    Starting the explorer section

--- src/components/terminal/partials/about/Sidebar.js
+++ src/components/terminal/partials/about/Sidebar.js
@@ -2,6 +2,12 @@ import React from 'react'
 
 export const Sidebar = () => {
   return (
-    <div className='sidebar'>Sidebar</div>
+    <div className='sidebar'>
+      Explorer: <i class="fa-solid fa-folder-tree"></i>
+      <ul>
+        <li></li>
+      </ul>
+
+    </div>
   )
 }
