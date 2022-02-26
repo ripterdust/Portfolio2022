c72afa6b34cee4d063377cb4e33690158504723b
ripterdust <bryantello2010@hotmail.com>
2022-02-26 15:01:46 -0600

    Added the text

--- src/components/terminal/partials/about/AboutMe.js
+++ src/components/terminal/partials/about/AboutMe.js
@@ -2,6 +2,15 @@ import React from 'react'
 
 export const AboutMe = () => {
   return (
-    <div>AboutMe</div>
+    <div className='aboutMe'>
+
+        <div className="text">
+            * About Me <br />
+            * Hi, I'm Bryan. A multi-talent human with <br />
+            * More of 3 years of experience working with <br />
+            * Frontend and backend technologies
+        </div>
+
+    </div>
   )
 }
