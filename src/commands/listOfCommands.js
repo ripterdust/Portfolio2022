f1d7c7dd472451c027c8cce65284a16d33491148
ripterdust <bryantello2010@hotmail.com>
2022-02-26 18:27:47 -0600

    Updates

--- src/commands/listOfCommands.js
+++
@@ -1,1 +0,0 @@
-export const list =  ['hello', 'help', 'fakefetch', 'date', 'social', 'cv', 'curriculum', 'resume', 'github', 'linkedin', 'clear']
--- src/helpers/taskHelper.js
+++
@@ -1,7 +0,0 @@
-export const startMenu = ( ) => {
-    const menu = document.querySelector('#menu').classList;
-    console.log(menu)
-    menu.toggle('close');
-    menu.toggle('open')
-
-}
