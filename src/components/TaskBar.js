ea6a56550adf75835aad0e39da50f65856c4152c
ripterdust <bryantello2010@hotmail.com>
2022-03-16 00:45:43 -0600

    Added the maximize function

--- src/components/TaskBar.js
+++ src/components/TaskBar.js
@@ -1,24 +1,22 @@
-import React from 'react'
-import { startMenu } from '../helpers/taskHelper'
-import { StartMenu } from './Desktop/StartMenu'
+import React from "react";
+import { maximize } from "../helpers/maximize";
+import { startMenu } from "../helpers/taskHelper";
+import { StartMenu } from "./Desktop/StartMenu";
 
 export const TaskBar = () => {
-
-
   return (
-    <div className='taskbar'>
-      <div className="icon start" onClick={ startMenu }>
+    <div className="taskbar">
+      <div className="icon start" onClick={startMenu}>
         <i className="fa-brands fa-windows so-icon"></i>
-        <StartMenu/>
-
+        <StartMenu />
       </div>
-      <div id='task-terminal' className='icon'>
-        <i className="fa-solid fa-terminal terminal-icon-logo" ></i>
+      <div id="task-terminal" className="icon" onClick={() => maximize()}>
+        <i className="fa-solid fa-terminal terminal-icon-logo"></i>
       </div>
 
       <div className="hiden" id="task-files">
         <i className="fa-solid fa-folder folder"></i>
       </div>
     </div>
-  )
-}
+  );
+};
