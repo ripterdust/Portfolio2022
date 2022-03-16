602dbc9fa67b19fa136e5808845ec6e68e0e65de
ripterdust <bryantello2010@hotmail.com>
2022-03-16 01:06:54 -0600

    Added the main start menu styles

--- src/components/Desktop/StartMenu.js
+++ src/components/Desktop/StartMenu.js
@@ -5,6 +5,7 @@ export const StartMenu = () => {
     <div className="startMenu close" id="menu">
       <div className="Bg">
         <i className="fa-solid fa-power-off"></i>
+        Turn off
       </div>
     </div>
   );
