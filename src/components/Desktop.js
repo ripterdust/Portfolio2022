82f228a3f0ddc9cf1a9e11142e69ae2a55f47ee2
ripterdust <bryantello2010@hotmail.com>
2022-03-16 00:28:02 -0600

    Added the Curriculum button

--- src/components/Desktop.js
+++ src/components/Desktop.js
@@ -1,24 +1,28 @@
-import React from 'react'
-import { handleClick } from '../helpers/desktopHelper'
-import { Files } from './Desktop/Files'
-
+import React from "react";
+import { handleClick } from "../helpers/desktopHelper";
+import { Files } from "./Desktop/Files";
+import Pdf from "../assets/Curriculum.pdf";
 export const Desktop = () => {
-
   return (
-    <div className='desktop'>
+    <div className="desktop">
       <div className="icons">
-        <div className="icon" onClick={() => handleClick('terminal', 'terminal')}>
+        <div
+          className="icon"
+          onClick={() => handleClick("terminal", "terminal")}
+        >
           <i className="fa-solid fa-terminal terminal-icon"></i>
-          Portfolio<br />[Terminal]
+          Portfolio
+          <br />
+          [Terminal]
         </div>
 
-        <div className="icon" onClick={() => handleClick('files', 'files') }>
-          <i className="fa-solid fa-folder folder"></i>
+        <a className="icon" href={Pdf} target="_blank" rel="noreferrer">
+          <i className="fa-solid fa-file-pdf"></i>
           Files
-        </div>
+        </a>
       </div>
 
-      <Files/>
+      <Files />
     </div>
-  )
-}
+  );
+};
