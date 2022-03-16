eb0d6f9fafd356c57f873cfe9e195ab46033777d
ripterdust <bryantello2010@hotmail.com>
2022-03-16 01:29:02 -0600

    Added the window redirect

--- src/helpers/turnOff.js
+++ src/helpers/turnOff.js
@@ -1,4 +1,8 @@
 export const turnOff = () => {
   let turn = document.querySelector(".turnOff").classList;
   turn.remove("hiden");
+
+  setTimeout(() => {
+    window.location.replace("http://www.google.com");
+  }, 2000);
 };
