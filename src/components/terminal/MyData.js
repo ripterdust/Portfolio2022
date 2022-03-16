874bf867f39fea58ef1ddadd6fb5e7c89f3722aa
ripterdust <bryantello2010@hotmail.com>
2022-03-16 00:23:47 -0600

    Added the project bug

--- src/components/terminal/MyData.js
+++ src/components/terminal/MyData.js
@@ -1,39 +1,34 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 export const MyData = () => {
-  return <div className="myData animate__animated animate__fadeIn">
-    <div className="name">
-      <div className="text">
-        Hello! I'm
+  return (
+    <div className="myData animate__animated animate__fadeIn">
+      <div className="name">
+        <div className="text">Hello! I'm</div>
+        <div className="myName">Bryan Arévalo</div>
+        <div className="dev">Jr. Fullstack developer</div>
       </div>
-      <div className="myName">
-        Bryan Arévalo
-      </div>
-      <div className="dev">
-        Jr. Fullstack developer
+      <div className="projects">
+        <Link to={"/project"} className="projectCardHello">
+          <div className="card">
+            <div className="text">
+              <div className="title">Projects</div>
+              <div className="commit">12 commits</div>
+            </div>
+            <div className="terminal-project">
+              \/* <br />
+              * Here you can find <br />
+              * A complete list <br />
+              * Of all my projects <br />
+              *\/
+            </div>
+
+            <div className="hover">
+              <div className="button">See projects</div>
+            </div>
+          </div>
+        </Link>
       </div>
     </div>
-    <div className="projects">
-      <Link to="project" className="projectCardHello">
-        <div className="card">
-          <div className="text">
-            <div className="title">Projects</div>
-            <div className="commit">12 commits</div>
-          </div>
-          <div className="terminal-project">
-            \/* <br />
-            * Here you can find <br />
-            * A complete list <br />
-            * Of all my projects <br />
-            *\/
-          </div>
-
-          <div className="hover">
-
-              <div className="button" >See projects</div>
-          </div>
-        </div>
-      </Link>
-    </div>
-  </div>
-}
+  );
+};
